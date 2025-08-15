56dccbdc7454723efc2b730db6f705cd33178eac
ygortsf <ygordetarcio@gmail.com>
2025-08-15 17:51:02 +0000

    commit teste

---
+++ test-next/app/page.js
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+import { Galera } from "@/gella/Galera";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Texto descritivo</h2>
+      < Galera/>
+    </div>
+  );
+}
